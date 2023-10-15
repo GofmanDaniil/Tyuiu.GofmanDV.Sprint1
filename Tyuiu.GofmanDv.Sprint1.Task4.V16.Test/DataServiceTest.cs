259bac1d9ea6b7303a894e5af404083b87ac7492
GofmanDaniil <Tyuiu.GofmanDV@outlook.com>
2023-10-15 23:30:54 +0500

    Task4

---
+++ Tyuiu.GofmanDv.Sprint1.Task4.V16.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.GofmanDV.Sprint1.Task4.V16.Lib;
+namespace Tyuiu.GofmanDV.Sprint1.Task4.V16.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double res = ds.Calculate(1);
+
+            Assert.AreEqual(0.200, res, 0.001);
+        }
+    }
+}
---
+++ Tyuiu.GofmanDv.Sprint1.Task4.V16/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.GofmanDV.Sprint1.Task4.V16.Lib;
+namespace Tyuiu.GofmanDV.Sprint1.Task4.V16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число x");
+            double x, y;
+            x = double.Parse(Console.ReadLine());
+
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнил: Гофман Д. В. | СМАРТб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема:  Class Math                                                       *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #30                                                             *");
+            Console.WriteLine("* Выполнил: Гофман Даниил Викторович | СМАРТб-23-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные,*");
+            Console.WriteLine("*   вычисляет результат по формуле и печатает его на экране*");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("x = " + x);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(Math.Round(ds.Calculate(x), 3));
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
